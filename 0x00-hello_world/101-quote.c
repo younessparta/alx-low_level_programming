082aa00dcb26373c22aee32ff27043c0156296e4
younessparta <tlbmusic22@gmail.com>
2023-06-15 19:58:17 -0700

    #include <stdio.h>

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,14 @@
 #include <unistd.h>
 
 /**
-*main - Entry point
+* main - Entry point
 *
-*Return: Always 1 (Success)
+* Return: Always 1 (Success)
 */
 int main(void)
 {
 	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
