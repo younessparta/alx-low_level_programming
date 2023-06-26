495e5eaa3219572a7ff6b4983070be80b60084aa
younessparta <tlbmusic22@gmail.com>
2023-06-26 13:44:19 -0700

    #include "main.h"

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+
+main - check the code
+
+Return: Always 0.
+*/
 int main(void)
 {
-	int n;
+int n;
 
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
+n = 402;
+printf("n=%d\n", n);
+reset_to_98(&n);
+printf("n=%d\n", n);
+return (0);
 }
