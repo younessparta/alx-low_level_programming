2e2495c6dc8782957aa36a5449d2d871c3938f31
younessparta <tlbmusic22@gmail.com>
2023-06-29 17:20:55 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,9 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p + 7) = 98; printf("a[2] = %d\n", *(p + 5));
+  *(p + 7) = 98; // Dereference p with an offset of 7 and assign 98 to its value
 
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
   return 0;
 }
