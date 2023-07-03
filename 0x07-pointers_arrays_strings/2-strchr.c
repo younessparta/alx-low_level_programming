e10713c48242732660050137269a6a8984e09d52
younessparta <tlbmusic22@gmail.com>
2023-07-03 16:11:14 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates the first occurrence of a character in a string
+ * @s: Input string
+ * @c: Character to be located
+ *
+ * Return: Pointer to the first occurrence of the character c in the string s,
+ *         or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
