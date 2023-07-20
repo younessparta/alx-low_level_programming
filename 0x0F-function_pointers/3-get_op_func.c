a52aad0301d2fb74adad2b70aa0258b4327797df
younessparta <tlbmusic22@gmail.com>
2023-07-19 18:43:04 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,7 +3,7 @@
 /**
  * get_op_func - Selects the correct function to perform
  *               the operation asked by the user.
- * @s: The operator passed as an argument.
+ * @s: The operator passed as argument.
  *
  * Return: A pointer to the function corresponding
  *         to the operator given as a parameter.
@@ -16,7 +16,7 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL},
+		{NULL, NULL}
 	};
 
 	int i = 0;
